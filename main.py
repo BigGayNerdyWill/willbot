4e50b3379def506f0abb81ba430e0c7b704226c4
BigGayNerdyWill <will@ponsonby.org>
2020-05-24 02:04:32 +0100

    started working on a tensorflow based model so will need toredo the todo list but the goals are somewhat the same

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 import re
 
 
-
 f = open("data/data.txt")
 words = []
 wrd2idx = {}
@@ -28,8 +27,6 @@ def text2words(text):
             else:
                 return text
     
-
-
 i=0
 l = f.readline()
 while l:
@@ -49,10 +46,13 @@ idx2wrd = np.array(words)
 
 data = [[x[0],x[1]] for x in re.findall(r"\[\[([^\]]+)\]([^\]]+)\]",text)]
 
+
 def sigmoid(nump):
     return 1/(1+np.exp(-nump))
 
-#forward progagaion
+def sigmoidGradient(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
 def lstm_cell_forward(Slt,Alt,Xt,Wf,bf,Wu,bu,Ws,bs,Wo,bo):
 
     concat = np.concatenate((Alt,Xt),axis=0)
@@ -67,9 +67,33 @@ def lstm_cell_forward(Slt,Alt,Xt,Wf,bf,Wu,bu,Ws,bs,Wo,bo):
     return [Ct_temp,Fu,Ff,Fo,Ct,At]
 
 
-def nearby_word(emb_mat,onehot,W,b):
-    return sigmoid(np.dot(W,(np.dot(emb_mat,onehot))) + b)
+def split(data,splits):
+    splits= [round(x*len(data)/100) for x in splits]
+    j=0
+    for i in range(len(splits)):
+        j+=splits[i]
+        splits[i] = j
+    train = data[:splits[0]]
+    test = data[splits[0]:splits[1]]
+    dev = data[splits[1]:splits[2]]
+    return {"train":train,"test":test,"dev":dev}
 
-#backward progagation
 
-#learning
+import tensorflow as tf
+
+import numpy as np
+import os
+import time
+
+tf.enable_eager_execution()
+
+examples_per_epoch = 200
+text_as_int = np.array([wrd2idx[w] for w in text2words(text)])
+
+# Create training examples / targets
+wrd_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+for i in wrd_dataset.take(5):
+  print(idx2wrd[i.numpy()])
+
+sequences = char_dataset.batch(, drop_remainder=True)
--- tests.py
+++ tests.py
@@ -1,2 +1,15 @@
-a= [1,2,3,4,5,6]
-print(a.index())
+def split(data,splits):
+    splits= [round(x*len(data)/100) for x in splits]
+    j=0
+    for i in range(len(splits)):
+        j+=splits[i]
+        splits[i] = j
+    train = data[:splits[0]]
+    test = data[splits[0]:splits[1]]
+    dev = data[splits[1]:splits[2]]
+    return {"train":train,"test":test,"dev":dev}
+
+a=[x for x in range(137)]
+a = split(a,[80,10,10])
+for i,u in a.items():
+    print("{0} : {1}, {2}".format(i,u,len(u)))
